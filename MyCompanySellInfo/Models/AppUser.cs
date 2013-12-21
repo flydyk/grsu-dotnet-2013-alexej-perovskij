84909a4be90a707a477302b436c865baeca1e92a
de4rmInc <perovskijab@gmail.com>
2013-12-21 16:38:07 +0300

    added user managment by superadmin

---
+++ MyCompanySellInfo/Models/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyCompanySellInfo.Models
+{
+    public class AppUserModel
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }
+
+    }
+}
